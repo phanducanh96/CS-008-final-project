60fe1fc4bbd3ee516822b53122721150b52bd1f3
phanducanh96 <aphan@uvm.edu>
2015-11-10 10:44:26 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+    <table>
+        <tr id="row1">
+            <th>UVM Theatre</th>
+            <th></th>
+            <th></th>
+        </tr>
+        <tr id="row2">
+            <th>About Us</th>
+            <th>Get Invovle</th>
+            <th>Photos</th>
+        </tr>
+        <tr id="row3">
+            <td><a href="mission.php">Our Mission</a></td>
+            <td><a href="http://www.uvm.edu/theatre/">UVM Theater Department</a></td>
+            <td><a href="oldpic.php">Past show</a></td>
+        </tr>
+        <tr id="row4">
+            <td><a href="theatre.php">Who are we?</a></td>
+            <td><a href="scholar.php">Award & Scholarship</a></td>
+            <td><a href="recentpic.php">Recent show</a></td>
+        </tr>
+        <tr id="row5">
+            <td><a href="seating.php">Seating Chart</a></td>
+            <td><a href="audition.php">Audition</a></td>
+            <td></td>
+        </tr>
+        <tr id="row6">
+            <td><a href="access.php">Accessibility</a></td>
+            <td><a href="volunteer.php">Ushering/Volunteering</a></td>
+            <td></td>
+        </tr>
+        <tr id="row7">
+            <td><a href="direction.php">Direction & Parking</a></td>
+            <td><a href="advertise.php">Advertise</a></td>
+            <td></td>
+        </tr>
+        <tr id="row8">
+            <td><a href="contact.php">Contact Us</a></td>
+            <td><a href="sponsor.php">Our Sponsors</a></td>
+            <td></td>
+        </tr>
+    </table>
+</footer>
