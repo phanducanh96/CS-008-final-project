4b4a4bce2a80ff5a60e7f537bab2639d43fcf8a0
XuanyiZhu <xzhu2@uvm.edu>
2015-11-09 22:28:33 -0500

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,10 @@
+<!-- ######################     Main Navigation   ########################## -->
+<nav>
+    <ol>
+        <li><figure>
+                    <img class ="navLogo" src = "img/logo.png" alt = "uvm theatre department logo" />
+            </figure></li>
+        <li class = 'nav'><a href="index.php/#PopularShow">Popular Show</a></li>
+        <li class = 'nav'><a href="index.php/#ComingSoon">Coming Soon</a></li>
+    </ol>
+</nav>
