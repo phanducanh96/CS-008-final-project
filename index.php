f59bd4e2e8b063a30404ec764c68571dd2b05f49
XuanyiZhu <xzhu2@uvm.edu>
2015-11-09 22:29:02 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include'top.php';
+?>
+/* 
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+
+<h2 id ="PopularShow">Popular Show</h2>
+<h2 id = "ComingSoon">Coming Soon</h2>
+        </body>
+        </html>
