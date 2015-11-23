3c97e64ed61fddebd145e4fb1765eebc1b7c24a4
phanducanh96 <aphan@uvm.edu>
2015-11-24 05:51:22 +0700

    Create footer2.php

---
+++ footer2.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="footer ph1">
+        <div class="container">
+            <div class="row">
+                <div class="row1">
+                    <h3 class="smallheading1">About Us</h3>
+                    <ol class="list_footer_1">
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=25"</a>Our Mission</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=26"</a>Who are we?</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=27"</a>Seating Chart</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=28"</a>Accessibility</a></li>
+                        <li><a href="direction.php"</a>Direction & Parking</a></li>
+                        <li><a href="contact.php"</a>Contact Us</a></li>
+                    </ol>
+                </div>
+                <div class="row2">
+                    <h3 class="smallheading2">Get Involve</h3>
+                    <ol class="list_footer_2">
+                        <li><a href="http://www.uvm.edu/theatre/">UVM Theater Department</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=28">Award & Scholarship</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=29">Auditions</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=30">Ushering/Voluntering</a></li>
+                        <li><a href="https://aphan.w3.uvm.edu/cs008/finalproject/access.php?id=31">Advertise</a></li>
+                        <li><a href="sponsor.php">Our Sponsors</a></li>
+                        
+                    </ol>
+                </div>
+                <div class="row3">
+                    <h3 class="smallheading3">Photo</h3>
+                    <ol class="list_footer_2">
+                        <li><a href="oldphoto.php"> Past Photos</a></li>
+                        <li><a href="newphoto.php">New Photos</a></li>
+                    </ol>
+                </div>
+        </div>
+    </div>
+</footer>
