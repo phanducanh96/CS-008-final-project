fd119ecf4607cfdf1c0d6a8f878d3f5326b21d15
phanducanh96 <aphan@uvm.edu>
2015-11-24 03:27:51 +0700

    Update show.php

--- show.php
+++ show.php
@@ -4,7 +4,7 @@ include ('top.php');
 <?php
 print "<ol>";
 foreach ($records as $oneRecord) {
-    if ($oneRecord[1] == "past"){
+    if ($oneRecord[1] == "past") {
         if ($oneRecord[0] == $showId) {
             print "\n\t<li>";
             print "<h1>" . $oneRecord[2] . "</h1>";
@@ -14,7 +14,22 @@ foreach ($records as $oneRecord) {
             print "<p>" . $oneRecord[9] . "</p>";
 
             print "\n\t</li>";
+        }
     }
+}
+print "<ol>";
+foreach ($records as $oneRecord) {
+    if ($oneRecord[1] == "now") {
+        if ($oneRecord[0] == $showId) {
+            print "\n\t<li>";
+            print "<h1>" . $oneRecord[2] . "</h1>";
+            print '<img src="' . $oneRecord[8] . '">';
+            print "<p>" . $oneRecord[2] . "</p>";
+            print "<p>" . $oneRecord[5] . "</p>";
+            print "<p>" . $oneRecord[9] . "</p>";
+
+            print "\n\t</li>";
+        }
     }
 }
 
@@ -25,7 +40,20 @@ print "</ol>";
 print"<h2>You May Like:</h2>";
 print "<ol>";
 foreach ($records as $oneRecord) {
-    if ($oneRecord[1] == "past"){
+    if ($oneRecord[1] == "past") {
+        if ($oneRecord[0] != $showId) {
+            print "\n\t<li>";
+            print "<a href='show.php?id=" . $oneRecord[0] . "'>" . '<img src="' . $oneRecord[8] . '">' . "</a>";
+            print "\n\t</li>";
+        }
+    }
+}
+?>
+<?php
+print"<h2>You May Like:</h2>";
+print "<ol>";
+foreach ($records as $oneRecord) {
+    if ($oneRecord[1] == "now") {
         if ($oneRecord[0] != $showId) {
             print "\n\t<li>";
             print "<a href='show.php?id=" . $oneRecord[0] . "'>" . '<img src="' . $oneRecord[8] . '">' . "</a>";
@@ -37,6 +65,26 @@ print "</ol>";
 ?>
 <hr>
 <?php
+print "<h2>Upcoming Show:</h2>";
+print "<ol>";
+foreach ($records as $oneRecord) {
+    if ($oneRecord[1] == "upcoming") {
+        if ($oneRecord[0] != "upcomingId") {
+            print "\n\t<li>";
+            print "<a href='upcoming.php?id=" . $oneRecord[0] . "'>" . $oneRecord[2] . "</a>";
+            print "\n\t</li>";
+        }
+    }
+}
+print "</ol>";
+?>
+</article>
+<hr>
+<?php
+include("footer.php");
+?>
+</body>
+</html>
 print "<h2>Upcoming Show:</h2>";
 print "<ol>";
 foreach ($records as $oneRecord) {
